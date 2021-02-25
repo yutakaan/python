d4b1188216dad17850f1899cd52dbe838e6985b3
yutakaan <strange_chameleon@live.jp>
2021-02-25 22:23:37 +0900

    Renewal weather.py
    Get weather data from JMA

--- subfunc/jsonfunc.py
+++ subfunc/jsonfunc.py
@@ -1,16 +1,15 @@
 import requests
 
-WEATHER_URL = 'http://weather.livedoor.com/forecast/webservice/json/v1'
-SITE_ID = '110010'
+WEATHER_URL = 'https://www.jma.go.jp/bosai/forecast/data/forecast/110000.json'
+DATE_MODE = 1
+AREA_MODE = 1
 
-def getWeather(url, site_id):
-    payload = {'city': site_id}
-    response = requests.get(url, params=payload).json()
+def getWeather(url):
+    response = requests.get(url).json()
     return response
 
 TRAIN_URL='https://tetsudo.rti-giken.jp/free/delay.json'
-# 路線名を記載
-TRAIN_ROUTE = ''
+TRAIN_ROUTE = '武蔵野線'
 
 def getTrainDelay(url):
     response = requests.get(url).json()
--- weather.py
+++ weather.py
@@ -5,50 +5,72 @@ from subfunc import logfunc
 from subfunc import jsonfunc
 from subfunc import linefunc
 
+# set logger
 LOG_DIR = logfunc.log_dir
 LOG_TXT = os.path.join(LOG_DIR, 'weather.log')
+logger = logfunc.get_logger(__name__, LOG_TXT)
 
+# set weather func
 URL = jsonfunc.WEATHER_URL
-SITE_ID = jsonfunc.SITE_ID
+DATE_MODE = jsonfunc.DATE_MODE
+AREA_MODE = jsonfunc.AREA_MODE
 
+# set line func
 LINE_URL = linefunc.URL
 ACCESS_TOKEN = linefunc.ACCESS_TOKEN
 HEADERS = linefunc.HEADERS
 
-args = sys.argv
-MODE = int(args[1])
+logger.info('start weather.py')
 
-logger = logfunc.get_logger(__name__, LOG_TXT)
+# get weather data
+def getWeatherData():
+    logger.debug('start weather api')
+    tenki_data = jsonfunc.getWeather(URL)
+    logger.debug('end weather api')
 
-logger.info('天気取得処理開始')
-logger.debug('API実行')
-tenki_data = jsonfunc.getWeather(URL, SITE_ID)
-logger.debug('API終了')
+    # get weather info
+    m_title = tenki_data[0]['publishingOffice']
+    m_date = tenki_data[0]['timeSeries'][0]['timeDefines'][DATE_MODE]
+    m_date = m_date[0:10]
+    m_area = tenki_data[0]['timeSeries'][0]['areas'][AREA_MODE]['area']['name']
+    m_weather = tenki_data[0]['timeSeries'][0]['areas'][AREA_MODE]['weathers'][DATE_MODE]
 
-m_title = tenki_data['title']
-m_date = tenki_data['forecasts'][MODE]['date']
-m_telop = tenki_data['forecasts'][MODE]['telop']
-m_max = tenki_data['forecasts'][MODE]['temperature'].get('max')
-m_min = tenki_data['forecasts'][MODE]['temperature'].get('min')
-if m_max is None:
-    m_max = 'None'
-else:
-    m_max = tenki_data['forecasts'][MODE]['temperature']['max']['celsius']
+    # get precip
+    num = len(tenki_data[0]['timeSeries'][1]['timeDefines'])
+    l_precip = [0] *4
+    j = 0
 
-if m_min is None:
-    m_min = 'None'
-else:
-    m_min = tenki_data['forecasts'][MODE]['temperature']['min']['celsius']
+    for i in range(num):
+        tmp_date = tenki_data[0]['timeSeries'][1]['timeDefines'][i]
+        if m_date == tmp_date[0:10]:
+            l_precip[j] = tenki_data[0]['timeSeries'][1]['areas'][AREA_MODE]['pops'][j]
+            j = j + 1
 
-LINE_MESSAGE = '\n' + m_title + '\n' \
-          + '日付：' + m_date + '\n' \
-          + '天気：' + m_telop + '\n' \
-          + '最高気温：' + m_max + '\n' \
-          + '最低気温：' + m_min
+    # get temprature
+    m_max = tenki_data[0]['timeSeries'][2]['areas'][AREA_MODE]['temps'][1]
+    m_min = tenki_data[0]['timeSeries'][2]['areas'][AREA_MODE]['temps'][0]
 
-logger.debug('Line送信開始')
-logger.debug(m_title + ',' + m_date + ',' + m_telop + ',' + m_max + ',' + m_min)
-linefunc.pushLine(LINE_URL, ACCESS_TOKEN, HEADERS, LINE_MESSAGE)
-logger.debug('Line送信処理終了')
-logger.info('天気取得処理終了')
+    # make message
+    MESSAGE = '\n' + m_title + '\n' \
+              + m_area + 'の' + m_date + 'の天気'  + '\n' \
+              + '天気：' + m_weather + '\n' \
+              + '降水確率' + '\n' \
+              + '   0時〜6時：' + l_precip[0] + '%' + '\n' \
+              + '  6時〜12時：' + l_precip[1] + '%' + '\n' \
+              + ' 12時〜18時：' + l_precip[2] + '%' + '\n' \
+              + ' 18時〜24時：' + l_precip[3] + '%' + '\n' \
+              + '最高気温：' + m_max + '度' + '\n' \
+              + '最低気温：' + m_min + '度'
 
+    logger.debug(m_title + ',' + m_area + ',' + m_date + ',' + m_weather + ',' + l_precip[0] + ',' + l_precip[1] + ',' + l_precip[2] + ',' + l_precip[3] + ',' + m_max + ',' + m_min)
+
+    return MESSAGE
+
+def main():
+    logger.info('start weather.py')
+    LINE_MESSAGE = getWeatherData()
+    linefunc.pushLine(LINE_URL, ACCESS_TOKEN, HEADERS, LINE_MESSAGE)
+    logger.info('end weather.py')
+
+if __name__=='__main__':
+    main()
